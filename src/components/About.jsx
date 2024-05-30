defbf552de183f2cee52471a0dfaee25c81381ed
amjedBllr <bamjed86@gmail.com>
2024-05-30 20:52:47 +0100

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,11 @@ import founder from '../assets/images/founder.png';
 
 const About = () => {
   return (
-    <div id="about" className="container mx-auto py-20 h-screen">
+    <div id="about" className="container mx-auto py-20">
       <div className="services flex flex-col items-start p-5">
-        <h3 className='font-bold text-secondary text-2xl p-5 pl-24'>Our Services</h3>
+        <h3 className='font-bold text-secondary text-2xl p-5 sm:pl-24 pl-5'>Our Services</h3>
         <br />
-        <div className='w-full flex justify-between px-10'>
+        <div className='w-full flex flex-col sm:flex-row justify-between sm:px-10'>
           <div class="card">
             <h4 className='font-semibold text-lg'>Secret Room Chats</h4>
             <p className='py-5'>Create private rooms with unique links and passwords. Only your invited friends can join</p>
@@ -27,13 +27,13 @@ const About = () => {
       </div>
 
       <div className="founder flex flex-col items-star p-5">
-        <h3 className='font-bold text-secondary text-2xl p-5 pl-24'>Our Founder</h3>
+        <h3 className='font-bold text-secondary text-2xl p-5 sm:pl-24 pl-5'>Our Founder</h3>
         <br />
-        <div className='w-full px-10'>
+        <div className='w-full sm:px-10'>
           <div class="founder-card">
             <h4 className='font-semibold text-lg'>Hey, I’m Amjed Bellir</h4>
             <p className='py-5 w-8/12'>the 21-year-old creator of BINATNA. As a full-stack web developer and IT grad, I’m all about making secure and fun chat platforms. <br/> Thanks for choosing My platform. Enjoy the privacy and fun. Reach out anytime with questions or feedback!</p>
-            <img src={founder} className="absolute right-28 top-5 h-60" />
+            <img src={founder} className="absolute right-28 top-5 h-60 -z-10 hidden sm:block" />
           </div>
         </div>
       </div>

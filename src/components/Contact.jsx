4010df07089f5e2e54efb9689a73e3191f7dd614
amjedBllr <bamjed86@gmail.com>
2024-06-01 03:00:26 +0100

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
 
     return (
         <div className="h-screen mx-auto pt-24">
-            <div className='w-full h-full flex justify-center items-center px-5 py-10 text-white'>
+            <div className='w-full h-full flex justify-center items-center gap-x-20 px-5 py-10 text-white'>
 
                 <motion.div
                     className='contact-info overflow-hidden cursor-grab active:cursor-grabbing flex flex-col justify-start items-center gap-3 py-9 rounded-xl bg-secondary w-96 h-fit'
@@ -62,9 +62,9 @@ const Contact = () => {
                         backgroundSize: "100% 200%",
                     }}
                 >
-                    <h4 className="font-medium text-white text-xl p-5">Contact information</h4>
+                    <h4 className="font-medium text-white text-xl p-5 hover:text-black-100">Contact information</h4>
                     <div className='w-3/4 text-sm font-light pb-28'>
-                        <p className="w-full break-words pb-16">We'd love to hear from you! Whether you have a question, feedback, or just want to say hello.</p>
+                        <p className="w-full break-words pb-16 hover:text-black-100">We'd love to hear from you! Whether you have a question, feedback, or just want to say hello.</p>
                         <div className='flex justify-start items-center mt-2 pb-1 hover:text-black-100'>
                             <FontAwesomeIcon icon={faEnvelope} />
                             <p className='pl-5'>bamjed86@gmail.com</p>
@@ -92,16 +92,31 @@ const Contact = () => {
 
 
 
-                <div className='flex flex-col gap-5 h-5/6 w5/12 text-black-100'>
-                    <h4 className="font-medium">Send Message : </h4>
-                    <label for="email">Email</label>
-                    <input name="email" value={message.email} onChange={handleChange} />
-                    <label for="name">Name</label>
-                    <input name="name" value={message.name} onChange={handleChange} />
-                    <label for="message">Message</label>
-                    <textarea name='message' value={message.message} onChange={handleChange}></textarea>
-                    <input type="submit" value="Submit" />
-                </div>
+                <motion.div
+                drag
+                dragConstraints={{
+                    top: -150,
+                    left: -150,
+                    right: 150,
+                    bottom: 150,
+                }}
+                 className='flex flex-col justify-center items-center gap-5 h-fit w-5/12 text-white bg-secondary p-14 rounded-xl'>
+                    <h4 className="font-medium text-xl pb-10">Send Message</h4>
+                    <div className='flex flex-col w-full gap-2'>
+                    <label className='text-sm' for="email">Email</label>
+                    <input className='p-2 text-black-100 rounded-lg' name="email" value={message.email} onChange={handleChange} />
+                    </div>
+                    <div className='flex flex-col w-full gap-2'>
+                    <label className='text-sm' for="name">Name</label>
+                    <input className='p-2 text-black-100 rounded-lg'  name="name" value={message.name} onChange={handleChange} />
+                    </div>
+                    <div className='flex flex-col w-full gap-2 '>
+                    <label className='text-sm' for="message">Message</label>
+                    <textarea className='p-2 text-black-100 rounded-lg'  name='message' value={message.message} onChange={handleChange}></textarea>
+                    </div>
+                    
+                    <input className='w-full bg-tertiary mt-10 py-2 rounded-lg text-white cursor-pointer hover:bg-primary active:opacity-90' type="submit" value="Submit" />
+                </motion.div>
             </div>
         </div>
     );

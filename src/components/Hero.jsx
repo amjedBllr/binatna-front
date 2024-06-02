4fff1144a5c5ae8460de1241261dc4d0d75ece73
amjedBllr <bamjed86@gmail.com>
2024-06-02 04:08:06 +0100

    landing page done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ function Hero() {
       <div className='air air4'></div>
       <LandingNav />
       <div className='h-20' />
-      <div className="flex flex-col items-start min-h-screen p-20">
+      <div className="flex flex-col items-start min-h-screen p-10 sm:p-20">
         <h1 className="sm:text-5xl text-3xl font-bold text-white-200 mb-4">BINATNA <span className="text-tertiary">.</span></h1>
         <p className="sm:text-xl text-s font-semibold text-white-200 mb-4">Your secret spot for private chats with friends.</p>
         <p className="sm:text-lg text-s text-gray-300 mb-8">Invite friends to private chat rooms or chat with our friendly bots when you're alone.</p>
